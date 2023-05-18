853fcbb7638bd9ec66cb02bed3db90aedbc26bf9
dophin7878 <130680931+dophin7878@users.noreply.github.com>
2023-05-18 11:41:22 +0200

    Delete src directory

--- src/main/java/com/mylive/springcodingsession/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mylive.springcodingsession.user;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer>{
-
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into user values(10001,sysdate(),'AB')
-insert into user values(10002,sysdate(),'Jack')
-insert into user values(10003,sysdate(),'Jill')
