02c3212f013c2c1c72c4c1c594254b6b83464fa5
Esteve-Volart <Octavi.Esteve-Volart@adesso.es>
2023-05-29 19:41:48 +0200

    Add Content-Type header; modify API path

--- src/main/java/com/mylive/springcodingsession/user/UserController.java
+++ src/main/java/com/mylive/springcodingsession/user/UserController.java
@@ -1,29 +1,30 @@
 package com.mylive.springcodingsession.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/myAPI")
 public class UserController {
 	
 	@Autowired
 	UserService userService;
 	
-	@GetMapping("/users")
+	@GetMapping(path= "/users", produces= MediaType.APPLICATION_JSON_VALUE)
 	public List<User> retrieveAllUsers() {
 		return userService.retrieveAllUsers();
 	}
 	
-	@GetMapping("/user/{id}")
+	@GetMapping(path="/user/{id}", produces= MediaType.APPLICATION_JSON_VALUE)
 	public Optional<User> retrieveUser(@PathVariable("id") int id) {
 		return userService.retrieveUser(id);
 	}
 
-	@PostMapping("/user")
+	@PostMapping(path="/user", produces= MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 		return userService.createUser(user);
 	}
