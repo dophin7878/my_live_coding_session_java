ccfe3e19f339eb0146e91bad657407034ab53e9d
Esteve-Volart <Octavi.Esteve-Volart@adesso.es>
2023-05-29 16:10:27 +0200

    Replace @RestController with @Service

--- src/main/java/com/mylive/springcodingsession/user/UserService.java
+++ src/main/java/com/mylive/springcodingsession/user/UserService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Service
 public class UserService {
 
 	@Autowired
