2d891c7f93958567d64bf2c86437b82b9272d611
Esteve-Volart <Octavi.Esteve-Volart@adesso.es>
2023-05-29 19:17:33 +0200

    Add Group reference in User

--- src/main/java/com/mylive/springcodingsession/user/User.java
+++ src/main/java/com/mylive/springcodingsession/user/User.java
@@ -1,9 +1,7 @@
 package com.mylive.springcodingsession.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.mylive.springcodingsession.group.Group;
+import javax.persistence.*;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -12,24 +10,27 @@ import java.time.LocalDate;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy= GenerationType.SEQUENCE)
 	private Integer id;
-	
+
 	@Size(min=2, message="Name should have at least 2 characters")
 	private String name;
-	
+
 	@Past
 	private LocalDate birthDate;
 
+	@ManyToOne(optional = false, fetch = FetchType.EAGER)
+	private Group group;
+
 	protected User() {
 	}
-	
+
 	public User(String name, LocalDate birthDate) {
 		super();
 		this.name = name;
 		this.birthDate = birthDate;
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
@@ -48,10 +49,17 @@ public class User {
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
-	
+	public Group getGroup() {
+		return group;
+	}
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "," +
+				"groupId=" + getGroup().getId() + ", groupName=" + getGroup().getName() + "]";
 	}
-	
+
 }
