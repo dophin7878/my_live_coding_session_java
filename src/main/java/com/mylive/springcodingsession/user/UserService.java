c21fbef53899de1936896cdd4fbac1cf05676440
Esteve-Volart <Octavi.Esteve-Volart@adesso.es>
2023-05-29 16:12:37 +0200

    Replace @RestController with @Service

--- src/main/java/com/mylive/springcodingsession/user/UserService.java
+++ src/main/java/com/mylive/springcodingsession/user/UserService.java
@@ -3,8 +3,7 @@ package com.mylive.springcodingsession.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
